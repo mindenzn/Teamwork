fef01751336bb20504fc1e32e454fc442d4450e8
MindenNZN <minde.ziur@gmail.com>
2020-10-16 11:54:21 +0300

    Added rooster gif with random text

--- Code PHP/TeamWork/index.php
+++ Code PHP/TeamWork/index.php
@@ -14,7 +14,7 @@ $random_name = $names[mt_rand(0, sizeof($names) - 1)];
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>TeamWork</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="main-background">
