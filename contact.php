89d59a8527af04d427a1c293fff05417e0ee72b5
MindenNZN <minde.ziur@gmail.com>
2020-10-16 19:13:32 +0300

    Pataisytos contact page clases is '_' i '-'

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ $background_image_rand = rand(1, 2);
     <title>Contact</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body class="background_img_contacts<?php print $background_image_rand; ?>">
+<body class="background-img-contacts<?php print $background_image_rand; ?>">
 <header>
     <nav class="navigation">
         <ul class="navigation-ul">
@@ -26,25 +26,25 @@ $background_image_rand = rand(1, 2);
     </nav>
 </header>
 <main>
-    <section class="contacts_container">
+    <section class="contacts-container">
         <h1 class="title-contact">Contact us "Višty-ydė ir Mindė"</h1>
         <ul>
-            <li class="list_contacts">
-                <img class="photo_img"
+            <li class="list-contacts">
+                <img class="photo-img"
                      src="https://scontent.fvno2-1.fna.fbcdn.net/v/t1.0-9/105016988_3229042567134035_5230164987909762088_n.jpg?_nc_cat=105&_nc_sid=09cbfe&_nc_ohc=erYvMof4afcAX9L-C3w&_nc_ht=scontent.fvno2-1.fna&oh=44882ff3c61c038c2eee354fb92f9db5&oe=5FAE3A91">
                 <span>Mindė: +370<?php print $random_number_Minde; ?></span>
             </li>
-            <li class="list_contacts">
-                <img class="photo_img"
+            <li class="list-contacts">
+                <img class="photo-img"
                      src="https://sadanduseless.b-cdn.net/wp-content/uploads/2020/05/funny-chickens-in-tutus21.jpg">
                 <span> Agnė: +370<?php print $random_number_Agne; ?></span>
             </li>
-            <li class="list_contacts">
-                <img class="photo_img" src="https://i.pinimg.com/474x/51/39/43/513943bf0e8a1e82369e048aca190d98.jpg">
+            <li class="list-contacts">
+                <img class="photo-img" src="https://i.pinimg.com/474x/51/39/43/513943bf0e8a1e82369e048aca190d98.jpg">
                 <span> Aistė: +370<?php print  $random_number_Aiste; ?></span>
             </li>
-            <li class="list_contacts">
-                <img class="photo_img"
+            <li class="list-contacts">
+                <img class="photo-img"
                      src="https://2.bp.blogspot.com/-y5o88uBdaos/T5PlQy4_bTI/AAAAAAAHgRI/6omkXRgZvJ4/s1600/funny+chickens,+chicken+photos,+funny+birds+photo+(1).jpg">
                 <span>Sigis: +370<?php print $random_number_Sigis; ?></span>
             </li>
