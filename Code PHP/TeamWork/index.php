b17363c7172104deaf6b44125cbe781d6352bc15
MindenNZN <minde.ziur@gmail.com>
2020-10-16 12:05:26 +0300

    Home page done

--- Code PHP/TeamWork/index.php
+++ Code PHP/TeamWork/index.php
@@ -29,6 +29,7 @@ $random_name = $names[mt_rand(0, sizeof($names) - 1)];
 <h1 class="title-text"> We are Vištidė ir Minde</h1>
 <img class="chicken-gif" src="https://i.pinimg.com/originals/ee/32/c1/ee32c1af78ec63ebb7760f979de567aa.gif" alt="">
 <img class="rooster-gif" src="https://img1.picmix.com/output/stamp/normal/5/6/6/2/292665_760d0.gif" alt="">
-<p> Šiuo metu laisvas kiaušrakulas: <?php print $random_name?></p>
+<p class="random-name-text">Šiuo metu laisvas kiaušrakulas: <span
+            class="random-name-bold"><?php print $random_name ?></span></p>
 </body>
 </html>
