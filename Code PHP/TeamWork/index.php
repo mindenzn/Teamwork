240d579a4c029cd31c33ed19805904ad3a981960
MindenNZN <minde.ziur@gmail.com>
2020-10-16 12:13:11 +0300

    Home page pataisyta

--- Code PHP/TeamWork/index.php
+++ Code PHP/TeamWork/index.php
@@ -18,18 +18,22 @@ $random_name = $names[mt_rand(0, sizeof($names) - 1)];
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="main-background">
-<nav class="navigation">
-    <ul class="navigation-ul">
-        <li class="navigation-list">Home<a href="teamwork.lt"></a></li>
-        <li class="navigation-list">Kiaušrakulas<a href="teamwork.lt/kiausrakulas.php"></a></li>
-        <li class="navigation-list">About<a href="teamwork.lt/about.php"></a></li>
-        <li class="navigation-list">Contact<a href="teamwork.lt/contact.php"></a></li>
-    </ul>
-</nav>
-<h1 class="title-text"> We are Vištidė ir Minde</h1>
-<img class="chicken-gif" src="https://i.pinimg.com/originals/ee/32/c1/ee32c1af78ec63ebb7760f979de567aa.gif" alt="">
-<img class="rooster-gif" src="https://img1.picmix.com/output/stamp/normal/5/6/6/2/292665_760d0.gif" alt="">
-<p class="random-name-text">Šiuo metu laisvas kiaušrakulas: <span
-            class="random-name-bold"><?php print $random_name ?></span></p>
+<header>
+    <nav class="navigation">
+        <ul class="navigation-ul">
+            <li class="navigation-list">Home<a href="teamwork.lt"></a></li>
+            <li class="navigation-list">Kiaušrakulas<a href="teamwork.lt/kiausrakulas.php"></a></li>
+            <li class="navigation-list">About<a href="teamwork.lt/about.php"></a></li>
+            <li class="navigation-list">Contact<a href="teamwork.lt/contact.php"></a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <h1 class="title-text"> We are Vištidė ir Minde</h1>
+    <img class="chicken-gif" src="https://i.pinimg.com/originals/ee/32/c1/ee32c1af78ec63ebb7760f979de567aa.gif" alt="">
+    <img class="rooster-gif" src="https://img1.picmix.com/output/stamp/normal/5/6/6/2/292665_760d0.gif" alt="">
+    <p class="random-name-text">Šiuo metu laisvas kiaušrakulas: <span
+                class="random-name-bold"><?php print $random_name ?></span></p>
+</main>
 </body>
 </html>
