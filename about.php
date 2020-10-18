0dfce483f909e70349889954b95e71736a1f8b79
MindenNZN <minde.ziur@gmail.com>
2020-10-18 20:04:57 +0300

    vel poakeistas kiausiniu paveiksliukas

--- about.php
+++ about.php
@@ -1,27 +1,25 @@
 <?php
-$story = array(
-    "the chickens met the Minde and everything was jolly",
-    "the chicks were going put of town and saw Minde and decided to build a chicken coop for parties",
-    "the chicks had the best party and tequilla in the backyard and all the chicks get down in the yard,
-    and damn right - it is better than yours, and daamn righ, we have Minde!"
-);
-$random_story = $story[mt_rand(0, sizeof($story) - 1)];
+$time_left_s = 30 - (date('s') % 30);
 
-$time_left_s = 10 - (date('s') % 6);
-if ($time_left_s === 0) {
-    print $random_story;
+if (10 >= $time_left_s) {
+    $story = "the chickens met the Minde and everything was jolly";
+} elseif (20 <= $time_left_s && 30 >= $time_left_s) {
+    $story = "the chicks were going put of town and saw Minde and decided to build a chicken coop for parties";
+} else {
+    $story = "the chicks had the best party and tequilla in the backyard and all the chicks get down in the yard, 
+    and damn right - it is better than yours, and daamn righ, we have Minde!";
 }
-$bg_img_rand = rand(1, 2);
+$background_img_rand = rand(1, 2);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv='refresh' content='1'>
     <title>About</title>
-    <meta http-equiv="refresh" content="1">
     <link rel="stylesheet" href="style.css">
 </head>
-<body class="background-img-about<?php print $bg_img_rand; ?>">
+<body class="background-img-about<?php print $background_img_rand; ?>">
 <header>
     <nav class="navigation">
         <ul class="navigation-ul">
@@ -32,9 +30,9 @@ $bg_img_rand = rand(1, 2);
         </ul>
     </nav>
 </header>
-<h1 class="title-text">We are Višty-ydė ir Mindė</h1>
+<h1 class="title-text">We are Vištidė ir Mindė</h1>
 <h2 class="title-text">How it works...</h2>
-<p class="title-para"><?php print $random_story; ?></p>
+<p class="title-para"><?php print $story; ?></p>
 <p class="title-para"><?php print $time_left_s; ?></p>
 </body>
 </html>
